7051e1e5b384413294d0b8880e80fb3d24fe0bfb
alihamidi5736 <30084639@lcwc.ac.uk>
2024-10-08 19:27:04 +0100

    Update task.py

--- task.py
+++ task.py
@@ -1,1 +1,11 @@
 # You will define and write each function for the task in this file
+
+def tempeture_conventor(tempture, unit):
+    if unit == "C" :
+        fahrenheit = (tempture * 9/5) + 32:
+        return f"{fahrenheit} F"
+      elif unit == "F":
+
+        celsius = (tempture - 32) * 5/9:
+        return f"{celsius} C"
+        else return "invalid unit"
